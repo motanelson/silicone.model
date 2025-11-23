7c2dd512b32b96884fc5b5009c9b2b81f4226689
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-11-23 16:16:58 +0000

    Delete riscv.sh

--- riscv.sh
+++
@@ -1,6 +0,0 @@
-
-printf "\033c\033[43;30m\n"
-riscv64-linux-gnu-gcc -lgcc -c  kernel.c -o kernel.o 
-riscv64-linux-gnu-as -c entry.S -o entry.o 
-riscv64-linux-gnu-ld -T linker.ld -nostdlib kernel.o entry.o -o kernel.elf 
-qemu-system-riscv64 -machine virt -bios none -kernel kernel.elf -serial mon:stdio 
